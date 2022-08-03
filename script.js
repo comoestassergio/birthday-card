622d2261bd6b43b0c15408d5d4933ec86e9eb8ea
sergio <dirtycrew666@gmail.com>
2022-08-03 17:16:07 +0500

    Connected start button

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 const cards = document.querySelectorAll('.card')
 const finalScreen = document.querySelector('.final-screen')
+const startBtn = document.querySelector('.start-btn')
 
 const msgSound = new Audio('/assets/msg-sound.mp4')
 const fnlScreeSound = new Audio('/assets/fnl-screen-sound.mp4')
 
-const DEFAULT_DELAY = 1000
+const DEFAULT_DELAY = 800
 let multiplier = 1
 
-cards.forEach(card => {
-    console.log(multiplier)
-    multiplier += multiplier
-    let delay = DEFAULT_DELAY * multiplier
-    console.log(delay)
-    setTimeout(function(){
-        makeVisible(card)
-        playSound(msgSound)
-    }, delay)
+startBtn.addEventListener('click', function(){
+    startBtn.classList.add('clicked')
+    startBtn.addEventListener('animationend', function(){
+        startBtn.style.display = 'none'
+
+        let delay
+        cards.forEach(card => {
+            multiplier += multiplier
+            delay = DEFAULT_DELAY * multiplier
+            setTimeout(function(){
+                makeVisible(card)
+                playSound(msgSound)
+            }, delay)
+        })
+
+        setTimeout(function(){
+            makeVisible(finalScreen)
+            playSound(fnlScreeSound)
+        }, delay + 3000)
+    })
 })
 
-setTimeout(function(){
-    makeVisible(finalScreen)
-    playSound(fnlScreeSound)
-}, 20000)
+
 
 function makeVisible(el){
     el.classList.toggle('visible')

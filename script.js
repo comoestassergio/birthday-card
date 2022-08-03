c18caa7bd13c9add1ec105ac74d633438acafccd
sergio <dirtycrew666@gmail.com>
2022-08-03 14:54:09 +0500

    Added full logic

---
+++ script.js
@@ -0,0 +1,33 @@
+const cards = document.querySelectorAll('.card')
+const finalScreen = document.querySelector('.final-screen')
+
+const msgSound = new Audio('./assets/msg-sound.mp4')
+const fnlScreeSound = new Audio('./assets/fnl-screen-sound.mp4')
+
+const DEFAULT_DELAY = 1000
+let multiplier = 1
+
+cards.forEach(card => {
+    console.log(multiplier)
+    multiplier += multiplier
+    let delay = DEFAULT_DELAY * multiplier
+    console.log(delay)
+    setTimeout(function(){
+        makeVisible(card)
+        playSound(msgSound)
+    }, delay)
+})
+
+setTimeout(function(){
+    makeVisible(finalScreen)
+    playSound(fnlScreeSound)
+}, 22000)
+
+function makeVisible(el){
+    el.classList.toggle('visible')
+}
+
+function playSound(sound){
+    sound.muted = false
+    sound.play()
+}
